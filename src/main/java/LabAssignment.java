0764a1a136c747f82676739b4678df5322a68540
UmarAftab <Umerqureshi800@gmail.com>
2020-07-20 17:29:13 +0500

    Created my project

---
+++ src/main/java/LabAssignment.java
@@ -0,0 +1,7 @@
+public interface LabAssignment {
+    public abstract Integer PrimeNumber(Integer number);
+    public abstract Integer NumberCheck(Integer number);
+    public abstract Double Average(Integer number[]);
+    public abstract Boolean palindromeString(String string1,String string2);
+    public abstract Integer Temperature(Integer number);
+}
